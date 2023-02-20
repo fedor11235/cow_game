bda885961c448025b783d9af0aec07c2cf6d699c
Fedor <fedoravdeev3@gmail.com>
2023-02-20 21:31:35 +0300

    add event click

--- static/script/script.js
+++ static/script/script.js
@@ -1,1 +1,16 @@
-console.log('Hi mister!!')
+document.addEventListener("DOMContentLoaded", handlerDOMContentLoaded)
+
+function handlerDOMContentLoaded() {
+  const cows = document.getElementsByClassName('cow')
+  const myScore = document.getElementById('my-score')
+
+  for(const cow of cows){ 
+    cow.addEventListener('click', handlerClickOnCow)
+  }
+
+  function handlerClickOnCow(event) {
+    event.target.classList.add('cow-disable')
+    event.target.dataset.cowNumber
+    myScore.innerHTML = ++myScore.innerHTML
+  }
+}
