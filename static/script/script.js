c30231aae3f15b6ff689e8b5878d6b605347fcd6
Fedor <fedoravdeev3@gmail.com>
2023-02-21 14:27:58 +0300

    add timer

--- static/script/script.js
+++ static/script/script.js
@@ -1,3 +1,6 @@
+let cowsWait = []
+const timerIncr = 1
+
 document.addEventListener("DOMContentLoaded", handlerDOMContentLoaded)
 
 function handlerDOMContentLoaded() {
@@ -10,7 +13,51 @@ function handlerDOMContentLoaded() {
 
   function handlerClickOnCow(event) {
     event.target.classList.add('cow-disable')
-    event.target.dataset.cowNumber
+    // const cowNumber = event.target.dataset.cowNumber
+    setTimer(event.target)
     myScore.innerHTML = ++myScore.innerHTML
   }
 }
+
+// TODO исправить таймер
+function setTimer(cowTarget) {
+  const timeBegan = new Date()
+  deadline = new Date(
+    timeBegan.getFullYear(),
+    timeBegan.getMonth(),
+    timeBegan.getDate(),
+
+    timeBegan.getHours(),
+    timeBegan.getMinutes() + timerIncr,
+    timeBegan.getSeconds(),
+    timeBegan.getMilliseconds()
+  );
+  cowsWait.push({cowTarget: cowTarget, time: deadline})
+}
+
+function checkTime() {
+  let timeNow = new Date()
+
+  let tempCowsWait = []
+
+  for(const cow of cowsWait) {
+    if(timeNow >= cow.time) {
+      cow.cowTarget.classList.remove('cow-disable')
+      cow.cowTarget.innerHTML = ''
+    } else {
+      timeLeft = new Date(cow.time - timeNow)
+      console.log(timeLeft)
+      const [y, hour, minutes, seconds] = [
+        timeLeft.getYear(),
+        timeLeft.getHours(),
+        timeLeft.getMinutes(),
+        timeLeft.getSeconds(),
+      ]
+      const timer = `${y}:${hour}:${minutes}:${seconds}`
+      cow.cowTarget.innerHTML = timer
+      tempCowsWait.push(cow)
+    }
+  }
+  cowsWait = tempCowsWait
+}
+window.setInterval( checkTime, 100);
